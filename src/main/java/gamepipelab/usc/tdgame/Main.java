5c0ed6f107aca833fac35a5c0dbae358e224ed8b
Rohnak <rmotwani@usc.edu>
2015-10-08 13:08:18 -0700

    Adding Monster Tower relation class

---
+++ src/main/java/gamepipelab/usc/tdgame/Main.java
@@ -0,0 +1,7 @@
+package gamepipelab.usc.tdgame;
+
+/**
+ * Created by ronak on 10/8/15.
+ */
+public class Main {
+}
---
+++ src/main/java/gamepipelab/usc/tdgame/specs/MonsterTower.java
@@ -0,0 +1,7 @@
+package gamepipelab.usc.tdgame.specs;
+
+/**
+ * Created by ronak on 10/8/15.
+ */
+public class MonsterTower {
+}
